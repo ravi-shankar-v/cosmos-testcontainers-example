3e0edee8cb29aa48bc0cffde22977df4a573d337
ravi-shankar-v <ravi.shankarv@live.com>
2022-07-08 10:09:30 +0530

    Initial commit

---
+++ src/test/java/com/example/testcontainers/cosmos/CosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers.cosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
